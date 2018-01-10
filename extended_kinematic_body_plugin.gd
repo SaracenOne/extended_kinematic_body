f6c6280b9a040b90b5ead2c10339cbe0a9eca6dc
Saracen <SaracenOne@gmail.com>
2018-01-10 07:52:14 +0000

    Use native SVG as icon.

--- extended_kinematic_body_plugin.gd
+++ extended_kinematic_body_plugin.gd
@@ -5,6 +5,6 @@ func get_name():
 	return "ExtendedKinematicBody"
 
 func _enter_tree():
-	add_custom_type("ExtendedKinematicBody","KinematicBody",preload("extended_kinematic_body.gd"),preload("extended_kinematic_body.png"))
+	add_custom_type("ExtendedKinematicBody", "KinematicBody", preload("extended_kinematic_body.gd"), preload("icon_extended_kinematic_body.svg"))
 func _exit_tree():
 	remove_custom_type("ExtendedKinematicBody")
