804d36b305695f643232f6a2d743a3929b188439
Saracen <SaracenOne@gmail.com>
2020-02-05 21:24:25 +0000

    Add typing

--- extended_kinematic_body_plugin.gd
+++ extended_kinematic_body_plugin.gd
@@ -1,10 +1,11 @@
 tool
 extends EditorPlugin
 
-func get_name(): 
+func get_name() -> String:
 	return "ExtendedKinematicBody"
 
-func _enter_tree():
+func _enter_tree() -> void:
 	add_custom_type("ExtendedKinematicBody", "KinematicBody", preload("extended_kinematic_body.gd"), preload("icon_extended_kinematic_body.svg"))
-func _exit_tree():
+
+func _exit_tree() -> void:
 	remove_custom_type("ExtendedKinematicBody")
